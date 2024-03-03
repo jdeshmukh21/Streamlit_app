6b71be67dd8d031f95de711c87afb33e8f77d73a
Jaspal Deshmukh <26095039-JaspalDeshmukh@users.noreply.replit.com>
2024-03-03 12:57:59 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,15 @@
+# from flask import Flask
+
+# app = Flask(__name__)
+
+# @app.route('/')
+# def home():
+#     return 'Welcome to my website!'
+
+# app.run(host='0.0.0.0')
+
+import streamlit as st
+
+st.title('Our First Streamlit App')
+st.subheader('Intrdocing stramlit in automation with python')
+st.write('This is our first app')
